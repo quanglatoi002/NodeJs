c3d8e37f16e61b1c25a7c7cecaa10b8212534a44
quang <quanglatoi002@gmail.com>
2022-11-27 14:57:32 +0700

    Create a token for register

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,6 @@
 import db from "../models";
 import bcrypt from "bcryptjs"; // thư viện dùng để băm mật khẩu
+import jwt from "jsonwebtoken";
 // hàm hashPassword
 const hashPassword = (password) =>
     bcrypt.hashSync(password, bcrypt.genSaltSync(10));
@@ -14,11 +15,24 @@ export const register = ({ email, password }) =>
                     password: hashPassword(password),
                 },
             });
+            // Tạo token để mã hóa id, email, role_Code với hạn là 5d
+            const token = response[1]
+                ? jwt.sign(
+                      {
+                          id: response[0].id,
+                          email: response[0].email,
+                          role_Code: response[0].role_Code,
+                      },
+                      process.env.JWT_SECRET,
+                      { expiresIn: "5d" }
+                  )
+                : null;
             res({
                 err: response[1] ? 0 : 1,
                 mes: response[1]
                     ? "Register is successfully"
                     : "Email already used",
+                token,
             });
             res({
                 err: 0,
